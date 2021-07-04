b9a76aaae8a485d0fa67b6c93d6da2416d78c037
ketpay <garciacardenas96@gmail.com>
2021-07-04 12:15:46 -0500

    modified:   beeproject2021/settings.py

--- beeproject2021/settings.py
+++ beeproject2021/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+
+from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
